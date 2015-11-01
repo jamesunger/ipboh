b0c55e2c0b945bfbeb476d4a64e394aee1ed2baa
James Unger <james.unger@gmail.com>
2015-11-01 01:08:22 -0400

    Cleanup command line arguments. Run gofmt.

--- ipboh.go
+++ ipboh.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"flag"
-	"io/ioutil"
-	"io"
-	"sync"
-	"os"
 	"bytes"
-	"time"
-	"strings"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	core "github.com/ipfs/go-ipfs/core"
+	corenet "github.com/ipfs/go-ipfs/core/corenet"
+	coreunix "github.com/ipfs/go-ipfs/core/coreunix"
+	importer "github.com/ipfs/go-ipfs/importer"
+	"github.com/ipfs/go-ipfs/importer/chunk"
+	peer "github.com/ipfs/go-ipfs/p2p/peer"
+	pin "github.com/ipfs/go-ipfs/pin"
+	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/armor"
 	"golang.org/x/crypto/ssh/terminal"
-        coreunix "github.com/ipfs/go-ipfs/core/coreunix"
-	pin "github.com/ipfs/go-ipfs/pin"
-	core "github.com/ipfs/go-ipfs/core"
-	"github.com/ipfs/go-ipfs/importer/chunk"
-	importer "github.com/ipfs/go-ipfs/importer"
-	corenet "github.com/ipfs/go-ipfs/core/corenet"
-	peer "github.com/ipfs/go-ipfs/p2p/peer"
-	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
-	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+	"sync"
+	"time"
 	//"github.com/ipfs/go-ipfs/blocks"
 	dag "github.com/ipfs/go-ipfs/merkledag"
 
-        bstore "github.com/ipfs/go-ipfs/blocks/blockstore"
-        bserv "github.com/ipfs/go-ipfs/blockservice"
-        syncds "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore/sync"
-        ds "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
-        offline "github.com/ipfs/go-ipfs/exchange/offline"
+	ds "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
+	syncds "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore/sync"
+	bstore "github.com/ipfs/go-ipfs/blocks/blockstore"
+	bserv "github.com/ipfs/go-ipfs/blockservice"
+	offline "github.com/ipfs/go-ipfs/exchange/offline"
 
-        ft "github.com/ipfs/go-ipfs/unixfs"
-
+	ft "github.com/ipfs/go-ipfs/unixfs"
 
 	"code.google.com/p/go.net/context"
 	dagutils "github.com/ipfs/go-ipfs/merkledag/utils"
@@ -48,7 +48,7 @@ type Entry struct {
 }
 
 type Add struct {
-	Name string
+	Name    string
 	Content []byte
 }
 
@@ -68,32 +68,28 @@ func runIndex(n *core.IpfsNode, ctx context.Context, index *Index, wg *sync.Wait
 		defer con.Close()
 
 		fmt.Printf("Connection from: %s\n", con.Conn().RemotePeer())
-		indexbytes,err := json.Marshal(index)
+		indexbytes, err := json.Marshal(index)
 		if err != nil {
 			panic(err)
 		}
-		count,err := con.Write(indexbytes)
+		count, err := con.Write(indexbytes)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("Wrote bytes:",count)
+		fmt.Println("Wrote bytes:", count)
 		con.Close()
 	}
 
 	wg.Done()
 
-
 }
-
 
 func NewMemoryDagService() dag.DAGService {
-        // build mem-datastore for editor's intermediary nodes
-        bs := bstore.NewBlockstore(syncds.MutexWrap(ds.NewMapDatastore()))
-        bsrv := bserv.New(bs, offline.Exchange(bs))
-        return dag.NewDAGService(bsrv)
+	// build mem-datastore for editor's intermediary nodes
+	bs := bstore.NewBlockstore(syncds.MutexWrap(ds.NewMapDatastore()))
+	bsrv := bserv.New(bs, offline.Exchange(bs))
+	return dag.NewDAGService(bsrv)
 }
-
-
 
 //var rootnode *dag.Node
 func newDirNode() *dag.Node {
@@ -104,8 +100,6 @@ func newDirNode() *dag.Node {
 		return rootnode
 	}*/
 }
-
-
 
 func runAdd(n *core.IpfsNode, ctx context.Context, index *Index, wg *sync.WaitGroup) {
 	list, err := corenet.Listen(n, "/pack/add")
@@ -124,7 +118,7 @@ func runAdd(n *core.IpfsNode, ctx context.Context, index *Index, wg *sync.WaitGr
 		defer con.Close()
 
 		fmt.Printf("Connection from: %s\n", con.Conn().RemotePeer())
-		readbytes,err := ioutil.ReadAll(con)
+		readbytes, err := ioutil.ReadAll(con)
 		// should not die here
 		if err != nil {
 			fmt.Println("Failed to ready all bytes: ", err)
@@ -132,39 +126,33 @@ func runAdd(n *core.IpfsNode, ctx context.Context, index *Index, wg *sync.WaitGr
 		}
 
 		newadd := &Add{}
-		err = json.Unmarshal(readbytes,newadd)
+		err = json.Unmarshal(readbytes, newadd)
 		if err != nil {
 			fmt.Println("Failed to unmarshal json: ", err)
 			continue
 		}
 
-
 		fmt.Println("They are adding:", newadd.Name)
 
-
 		/*
-		b := blocks.NewBlock(newadd.Content)
-		fmt.Println("Block:", b.Key().B58String())
-		
-		blockadd,err := n.Blocks.AddBlock(b)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(blockadd)
+			b := blocks.NewBlock(newadd.Content)
+			fmt.Println("Block:", b.Key().B58String())
+
+			blockadd,err := n.Blocks.AddBlock(b)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(blockadd)
 		*/
 
-
-                //dagnode := &dag.Node{Data: newadd.Content}
-                //dagadd, err := n.DAG.Add(dagnode)
-                //if err != nil {
-	//		panic(err)
-         //       }
-
-
+		//dagnode := &dag.Node{Data: newadd.Content}
+		//dagadd, err := n.DAG.Add(dagnode)
+		//if err != nil {
+		//		panic(err)
+		//       }
 
 		newdirnode := newDirNode()
 		e := dagutils.NewDagEditor(NewMemoryDagService(), newdirnode)
-
 
 		reader := bytes.NewReader(newadd.Content)
 		chnk, err := chunk.FromString(reader, "rabin")
@@ -172,33 +160,29 @@ func runAdd(n *core.IpfsNode, ctx context.Context, index *Index, wg *sync.WaitGr
 			panic(err)
 		}
 
-
 		dagnode, err := importer.BuildDagFromReader(
-                        n.DAG,
-                        chnk,
-                        importer.PinIndirectCB(n.Pinning.GetManual()),
-                )
+			n.DAG,
+			chnk,
+			importer.PinIndirectCB(n.Pinning.GetManual()),
+		)
 
-		err = e.InsertNodeAtPath(ctx,newadd.Name,dagnode,newDirNode)
+		err = e.InsertNodeAtPath(ctx, newadd.Name, dagnode, newDirNode)
 		if err != nil {
 			panic(err)
 		}
 
-                err = e.WriteOutputTo(n.DAG)
-                if err != nil {
+		err = e.WriteOutputTo(n.DAG)
+		if err != nil {
 			panic(err)
-                }
+		}
 
-
-
-		key,err := dagnode.Key()
+		key, err := dagnode.Key()
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println("Added:", key.B58String())
 		entry := Entry{Name: newadd.Name, Hash: key.B58String()}
-		index.Entries = append(index.Entries,&entry)
-
+		index.Entries = append(index.Entries, &entry)
 
 		rootnd := e.GetNode()
 
@@ -206,10 +190,10 @@ func runAdd(n *core.IpfsNode, ctx context.Context, index *Index, wg *sync.WaitGr
 		if err != nil {
 			panic(err)
 		}
-                mp := n.Pinning.GetManual()
-                mp.RemovePinWithMode(rnk, pin.Indirect)
-                mp.PinWithMode(rnk, pin.Recursive)
-                n.Pinning.Flush()
+		mp := n.Pinning.GetManual()
+		mp.RemovePinWithMode(rnk, pin.Indirect)
+		mp.PinWithMode(rnk, pin.Recursive)
+		n.Pinning.Flush()
 
 		fmt.Println("Pinned rn:", rnk)
 
@@ -219,11 +203,7 @@ func runAdd(n *core.IpfsNode, ctx context.Context, index *Index, wg *sync.WaitGr
 
 }
 
-
-
-
 func getEntryList(n *core.IpfsNode, target peer.ID) *Index {
-
 
 	index := makeIndex()
 	con, err := corenet.Dial(n, target, "/pack/index")
@@ -232,22 +212,18 @@ func getEntryList(n *core.IpfsNode, target peer.ID) *Index {
 		return index
 	}
 
+	//	rawbytes := make([]byte, 2048, 2048)
+	//	buf := bytes.NewBuffer(rawbytes)
+	//	_,err = io.Copy(buf, con)
 
-//	rawbytes := make([]byte, 2048, 2048)
-//	buf := bytes.NewBuffer(rawbytes)
-//	_,err = io.Copy(buf, con)
-
-
-
-	rawbytes,err := ioutil.ReadAll(con)
+	rawbytes, err := ioutil.ReadAll(con)
 	if err != nil {
 		fmt.Println(err)
 		return index
 	}
 	//fmt.Println("Got result:" + string(rawbytes))
 
-
-	err = json.Unmarshal(rawbytes,index)
+	err = json.Unmarshal(rawbytes, index)
 	if err != nil {
 		fmt.Println(err)
 		return index
@@ -258,19 +234,20 @@ func getEntryList(n *core.IpfsNode, target peer.ID) *Index {
 }
 
 func findKey(keyring openpgp.EntityList, name string) *openpgp.Entity {
-  for _, entity := range keyring {
-    for _, ident := range entity.Identities {
-      if strings.Contains(ident.Name, name) {
-        return entity
-      }
-    }
-  }
+	for _, entity := range keyring {
+		for _, ident := range entity.Identities {
+			if strings.Contains(ident.Name, name) {
+				return entity
+			}
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func decryptOpenpgp(data []byte, recipient string) ([]byte, error) {
-	privkeyfile,err := os.Open("/home/junger/.gnupg/secring.gpg")
+	home := os.Getenv("HOME")
+	privkeyfile, err := os.Open(fmt.Sprintf("%s/.gnupg/secring.gpg", home))
 	if err != nil {
 		fmt.Println("Failed to open secring", err)
 		return nil, err
@@ -281,7 +258,6 @@ func decryptOpenpgp(data []byte, recipient string) ([]byte, error) {
 		fmt.Println("Failed to open secring", err)
 		return nil, err
 	}
-
 
 	reader := bytes.NewReader(data)
 	block, err := armor.Decode(reader)
@@ -297,13 +273,17 @@ func decryptOpenpgp(data []byte, recipient string) ([]byte, error) {
 	fmt.Fprintln(os.Stderr, "")
 
 	privkey := findKey(privring, recipient)
-		if privkey.PrivateKey != nil && privkey.PrivateKey.Encrypted {
-			//fmt.Println("Decrypting private key using passphrase")
-			err := privkey.PrivateKey.Decrypt([]byte(passphrase))
-			if err != nil {
-				fmt.Println("Failed to decrypt key")
-			}
+	if privkey == nil {
+		return nil, errors.New("Associated private key not found.")
+	}
+
+	if privkey.PrivateKey != nil && privkey.PrivateKey.Encrypted {
+		//fmt.Println("Decrypting private key using passphrase")
+		err := privkey.PrivateKey.Decrypt([]byte(passphrase))
+		if err != nil {
+			fmt.Println("Failed to decrypt key")
 		}
+	}
 
 	for _, subkey := range privkey.Subkeys {
 		if subkey.PrivateKey != nil && subkey.PrivateKey.Encrypted {
@@ -314,20 +294,21 @@ func decryptOpenpgp(data []byte, recipient string) ([]byte, error) {
 		}
 	}
 
-	md,err := openpgp.ReadMessage(block.Body, privring, nil, nil)
+	md, err := openpgp.ReadMessage(block.Body, privring, nil, nil)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	plaintext,err := ioutil.ReadAll(md.UnverifiedBody)
+	plaintext, err := ioutil.ReadAll(md.UnverifiedBody)
 	if err != nil {
 		panic(err)
 	}
-	return plaintext,nil
+	return plaintext, nil
 }
 
 func encryptOpenpgp(data []byte, recipient string) ([]byte, error) {
-	pubkeyfile,err := os.Open("/home/junger/.gnupg/pubring.gpg")
+	home := os.Getenv("HOME")
+	pubkeyfile, err := os.Open(fmt.Sprintf("%s/.gnupg/pubring.gpg", home))
 	if err != nil {
 		fmt.Println("Failed to open pubring", err)
 		return nil, err
@@ -345,38 +326,33 @@ func encryptOpenpgp(data []byte, recipient string) ([]byte, error) {
 	w, _ := armor.Encode(buf, "PGP MESSAGE", nil)
 	plaintext, err := openpgp.Encrypt(w, []*openpgp.Entity{pubkey}, nil, nil, nil)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	reader := bytes.NewReader(data)
-	_,err = io.Copy(plaintext,reader)
+	_, err = io.Copy(plaintext, reader)
 	plaintext.Close()
 	w.Close()
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	return buf.Bytes(),nil
+	return buf.Bytes(), nil
 
 }
-
-
 
 func makeIndex() *Index {
 	entries := make([]*Entry, 0)
-	return &Index{ Entries: entries }
+	return &Index{Entries: entries}
 }
-
 
 func getNewContent(name string) Add {
 
-
-	rawbytes,err := ioutil.ReadAll(os.Stdin)
+	rawbytes, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
 
 	return Add{Name: name, Content: rawbytes}
-
 
 }
 
@@ -415,68 +391,50 @@ func main() {
 		panic(err)
 	}
 
-	//fmt.Println(n.Peerstore)
-	//ids := n.Peerstore.Peers()
-	//for i := range ids {
-	//	//fmt.Println(n.Peerstore.Peers[i])
-	//	fmt.Println(ids[i])
-	//}
-
-
-	//fmt.Println("Waiting for 30 secs")
-	//time.Sleep(30*time.Second)
-
-	//fmt.Println(n)
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	index := makeIndex()
 
-	var server,client,ping,verbose bool
-	var serverhash,add string
-	var dumphash,recipient string
-	flag.BoolVar(&server, "s", false, "Run as server")
+	var server, ping, verbose bool
+	var serverhash, add string
+	var catarg, recipient string
 	flag.BoolVar(&verbose, "v", false, "Verbose")
-	flag.BoolVar(&client, "c", false, "Run as client")
-	flag.StringVar(&dumphash, "d", "", "Dump contents of hash")
-	flag.StringVar(&recipient, "r", "", "PGP recipient")
-	flag.StringVar(&add, "a", "", "Add content")
+	flag.StringVar(&recipient, "r", "", "Encrypt or decrypt to PGP recipient")
 	flag.StringVar(&serverhash, "h", "", "Server hash to connect to")
 	flag.Parse()
 
-
 	server = hasCmd("server")
-	client = hasCmd("client")
 	ping = hasCmd("ping")
 	if hasCmd("add") {
 		add = getCmdArg("add")
 	}
 
 	if hasCmd("cat") {
-		dumphash = getCmdArg("cat")
+		catarg = getCmdArg("cat")
 	}
 
-
+	// pretty sure this is unnecessary?
 	if !n.OnlineMode() {
-		fmt.Println("Not online...\n")
-               return
-        }
-
-
+		fmt.Println("Not on online mode...\n")
+		return
+	}
 
 	if server {
-		go runIndex(n,ctx,index,&wg)
-		go runAdd(n,ctx,index,&wg)
+		go runIndex(n, ctx, index, &wg)
+		go runAdd(n, ctx, index, &wg)
 		wg.Wait()
+	} else if serverhash == "" {
+		fmt.Println("Need to specify a remote server node id e.g. -h QmarTZGZDhBpDY5wgx9qSJrFcNokF37iD44Vk2FTYGPyBs")
+		return
+
+	// client instance
 	} else {
-		//fmt.Println("I'll start up as a client connecting to:", serverhash)
 
 		target, err := peer.IDB58Decode(serverhash)
 		if err != nil {
 			panic(err)
 		}
-
-
 
 		if len(n.Peerstore.Addrs(target)) == 0 {
 			if verbose {
@@ -496,6 +454,7 @@ func main() {
 		}
 
 
+		// add something
 		if add != "" {
 
 			newcontent := getNewContent(add)
@@ -504,9 +463,8 @@ func main() {
 				return
 			}
 
-
 			if recipient != "" {
-				encbytes,err := encryptOpenpgp(newcontent.Content,recipient)
+				encbytes, err := encryptOpenpgp(newcontent.Content, recipient)
 				if err != nil {
 					fmt.Println("Failed to encrypt.")
 				}
@@ -520,31 +478,29 @@ func main() {
 			}
 			defer con.Close()
 
-
-			contentbytes,err := json.Marshal(newcontent)
-			countbytes,err := con.Write(contentbytes)
+			contentbytes, err := json.Marshal(newcontent)
+			countbytes, err := con.Write(contentbytes)
 			if err != nil {
 				panic(err)
 			}
 
 			if verbose {
 				fmt.Println("Wrote", countbytes, "bytes")
-				//fmt.Println(string(contentbytes))
 			}
 
-			//<- ctx.Done()
-			//fmt.Println("All set.")
+			time.Sleep(1 * time.Second)
 
-			time.Sleep(5*time.Second)
-		} else if dumphash != "" {
+		// cat something
+		} else if catarg != "" {
+			hash := ""
 			// FIXME: validate this in case there is a 46 len name!
 			foundhash := false
-			if len(dumphash) != 46 {
-				entrylist := getEntryList(n,target)
+			if len(catarg) != 46 {
+				entrylist := getEntryList(n, target)
 				//fmt.Println(entrylist)
 				for i := range entrylist.Entries {
-					if entrylist.Entries[i].Name == dumphash {
-						dumphash = entrylist.Entries[i].Hash
+					if entrylist.Entries[i].Name == catarg {
+						hash = entrylist.Entries[i].Hash
 						foundhash = true
 						break
 					}
@@ -558,29 +514,29 @@ func main() {
 				return
 			}
 
-			reader,err := coreunix.Cat(ctx, n, dumphash)
+			reader, err := coreunix.Cat(ctx, n, hash)
 			if err != nil {
 				panic(err)
 			}
 
-			bytes,err := ioutil.ReadAll(reader)
+			bytes, err := ioutil.ReadAll(reader)
 			if err != nil {
 				panic(err)
 			}
 
 			if recipient != "" {
 				//fmt.Println("orig", string(bytes))
-				bytes,err = decryptOpenpgp(bytes, recipient)
+				bytes, err = decryptOpenpgp(bytes, recipient)
 				if err != nil {
-					fmt.Println("Failed to decrypt:",err)
+					fmt.Println("Failed to decrypt:", err)
 					return
 				}
 			}
 
 			fmt.Println(string(bytes))
+
+		// ping remote server
 		} else if ping {
-
-
 
 			pings, err := n.Ping.Ping(ctx, target)
 			if err != nil {
@@ -588,7 +544,7 @@ func main() {
 				return
 			}
 			//fmt.Println(pings)
-			_,suc := <- pings
+			_, suc := <-pings
 			if !suc {
 				fmt.Println("Ping failed.")
 				return
@@ -596,35 +552,17 @@ func main() {
 
 			fmt.Println("Ping success.")
 
+
+		// fetch entry list by default
 		} else {
 
-
-			//entrylist := getEntryList(n,target)
-			//for k := 0; k < 15; k++ {
-			//	if len(entrylist.Entries) == 0 {
-			//		fmt.Println("Trying again.")
-					entrylist := getEntryList(n,target)
-			//	} else {
-			//		break
-			//	}
-			//}
+			entrylist := getEntryList(n, target)
 
 			for i := range entrylist.Entries {
 				fmt.Println(entrylist.Entries[i].Hash, entrylist.Entries[i].Name)
 			}
 
-			/*
-			con, err := corenet.Dial(n, target, "/pack/index")
-    			if err != nil {
-        			fmt.Println(err)
-        			return
-    			}
-
-    			io.Copy(os.Stdout, con)
-			*/
 		}
 	}
 
-
 }
-
